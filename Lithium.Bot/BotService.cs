e639f7e0b9f829a34c40c6250cc6e52babef9b09
Bubbies <66354328+bub-bl@users.noreply.github.com>
2025-12-21 21:00:35 +0100

    Setup discord bot connection

---
+++ Lithium.Bot/BotService.cs
@@ -0,0 +1,46 @@
+using Discord;
+using Discord.WebSocket;
+
+namespace Lithium.Bot;
+
+public sealed class BotService(
+    ILogger<BotService> logger,
+    IConfiguration config,
+    DiscordSocketClient client
+) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        client.Log += OnLogAsync;
+        client.Ready += OnReadyAsync;
+        client.MessageReceived += OnMessageReceivedAsync;
+
+        await client.LoginAsync(TokenType.Bot, config["Discord:Token"]);
+        await client.StartAsync();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private Task OnLogAsync(LogMessage log)
+    {
+        logger.LogInformation(log.ToString());
+        return Task.CompletedTask;
+    }
+
+    private Task OnReadyAsync()
+    {
+        logger.LogInformation($"{client.CurrentUser} connected!");
+        return Task.CompletedTask;
+    }
+
+    private async Task OnMessageReceivedAsync(SocketMessage message)
+    {
+        if (message.Author.Id == client.CurrentUser.Id) return;
+
+        if (message.Content == "!ping")
+            await message.Channel.SendMessageAsync("Pong!");
+    }
+}
---
+++ Lithium.Bot/Program.cs
@@ -0,0 +1,16 @@
+using Discord;
+using Discord.WebSocket;
+using Lithium.Bot;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<DiscordSocketClient>(sp => new DiscordSocketClient(new DiscordSocketConfig
+{
+    GatewayIntents = GatewayIntents.AllUnprivileged
+}));
+
+builder.Services.AddHostedService<BotService>();
+
+var app = builder.Build();
+
+app.Run();
